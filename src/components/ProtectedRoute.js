2bf1dda5f3a80b5bacc603d426e4ddecddd2f0c9
Daniel Chinemerem <danielchinemerem302@gmail.com>
2022-06-23 10:44:24 +0100

    fixed a bug for rerouting users that have been deleted from the firebase db

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -5,9 +5,12 @@ import HomePage from './HomePage';
 
 const ProtectedRoute = ({ children }) => {
   let { user } = useAuth();
-  // || localStorage.getItem('login') === 'true'
   if (!user) localStorage.removeItem('details');
-  return user ? children : <HomePage />; // here we are checking if user is logged in
+  return user || localStorage.getItem('login') === 'true' ? (
+    children
+  ) : (
+    <HomePage />
+  ); // here we are checking if user is logged in
 };
 
 export default ProtectedRoute;
